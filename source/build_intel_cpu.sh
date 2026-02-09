e3258bf01d0d96efd612c9547101224d6bc65521
Lauritskyst <lasta@kyst.dk>
2026-02-09 21:12:17 +0100

    Additional build files using openmp for gpu

---
+++ source/build_intel_cpu.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+LOGFILE=build_$(date +%Y%m%d_%H%M%S).log
+exec > >(tee "$LOGFILE") 2>&1
+
+echo "Starting build at $(date)"
+echo "Saving log to $LOGFILE"
+
+find . -name \*.m4|xargs dos2unix && find . -name \*.ac|xargs dos2unix && find . -name \*.am|xargs dos2unix
+find . -name \*.f90|xargs dos2unix
+find . -name \*.F90|xargs dos2unix
+find . -name \*.am|xargs dos2unix
+find . -name \*.sh|xargs dos2unix
+
+
+apt install -y libnetcdf-dev build-essential autoconf automake libtool pkg-config tzdata
+export CONFIG_SHELL=/bin/bash
+export IFXCFG=$(pwd)/ifx.cfg
+export IGC_EnableDPEmulation=1          # Enable float64 emulation during compilation
+export OverrideDefaultFP64Settings=1    # Enable double precision emulation at runtime - do this before running
+
+autoreconf -vif
+
+./autogen.sh
+
+./configure FCFLAGS="-O3" FC=ifx --disable-shared --prefix=/home/laurits-andreasen/sfincs/nvfortran_cpu_intel
+
+
+make clean
+
+make
+
+make install
+
+echo "Build finished at $(date)"
---
+++ source/build_nvfortran_cpu_omp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+LOGFILE=build_$(date +%Y%m%d_%H%M%S).log
+exec > >(tee "$LOGFILE") 2>&1
+
+echo "Starting build at $(date)"
+echo "Saving log to $LOGFILE"
+
+find . -name \*.m4|xargs dos2unix && find . -name \*.ac|xargs dos2unix && find . -name \*.am|xargs dos2unix
+find . -name \*.f90|xargs dos2unix
+find . -name \*.F90|xargs dos2unix
+find . -name \*.am|xargs dos2unix
+find . -name \*.sh|xargs dos2unix
+
+MANPATH=$MANPATH:/opt/nvidia/hpc_sdk/Linux_x86_64/25.11/compilers/man; export MANPATH
+PATH=/opt/nvidia/hpc_sdk/Linux_x86_64/25.11/compilers/bin:$PATH; export PATH
+
+
+apt install -y libnetcdf-dev build-essential autoconf automake libtool pkg-config tzdata
+export CONFIG_SHELL=/bin/bash
+
+autoreconf -vif
+
+./autogen.sh
+
+./configure FCFLAGS="-mp=multicore -Minfo=mp -stdpar=multicore -gpu=nomanaged -fast -O3 -gpu=ccall -DSIZEOF_PTRDIFF_T=999" FC=nvfortran --disable-shared --disable-openmp --prefix=/home/laurits-andreasen/sfincs/nvfortran_cpu_omp
+
+make clean
+
+make
+
+make install
+
+echo "Build finished at $(date)"
